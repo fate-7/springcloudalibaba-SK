90b67a9cba4142be51b34cddeb3028a3d12c05ef
fate-7 <2651074692@qq.com>
2020-04-09 17:58:31 +0800

    Ribbon规则全局配置

--- content-center/src/main/java/com/chengshare/contentcenter/configuration/UserCenterRibbonConfiguration.java
+++ content-center/src/main/java/com/chengshare/contentcenter/configuration/UserCenterRibbonConfiguration.java
@@ -1,6 +1,7 @@
 package com.chengshare.contentcenter.configuration;
 
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Configuration;
 import ribbonconfiguration.RibbonConfiguration;
 
@@ -18,5 +19,7 @@ import ribbonconfiguration.RibbonConfiguration;
 
 //@Configuration
 //@RibbonClient(name = "user-center", configuration = RibbonConfiguration.class)
+//全局配置
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 public class UserCenterRibbonConfiguration {
 }
