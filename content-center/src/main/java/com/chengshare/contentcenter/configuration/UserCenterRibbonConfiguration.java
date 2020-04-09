7ce1c785703c7c378771d5f9a04b41711275abd1
fate-7 <2651074692@qq.com>
2020-04-09 17:54:18 +0800

    Ribbon代码配置和属性配置的区别

--- content-center/src/main/java/com/chengshare/contentcenter/configuration/UserCenterRibbonConfiguration.java
+++ content-center/src/main/java/com/chengshare/contentcenter/configuration/UserCenterRibbonConfiguration.java
@@ -6,6 +6,12 @@ import ribbonconfiguration.RibbonConfiguration;
 
 /**
  * java配置类配置Ribbon的规则
+ * javaConfig：更加灵活，spring上下文的缺点，需要重新打包发布
+ * 配置文件：易上手，配置直观，无需重新打包，发布优先级更高
+ *
+ * 总结：
+ * 尽量使用属性配置
+ * 微服务尽量保持单一方式
  * @Author fate7
  * @Date 2020/4/9 4:08 下午
  **/
