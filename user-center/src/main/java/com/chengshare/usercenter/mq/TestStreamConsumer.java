db91d95f3cf47ef6c9463ec2fed11027f0119ada
fate-7 <2651074692@qq.com>
2020-04-13 19:43:48 +0800

    SpringCLoudStream全局异常处理

--- user-center/src/main/java/com/chengshare/usercenter/mq/TestStreamConsumer.java
+++ user-center/src/main/java/com/chengshare/usercenter/mq/TestStreamConsumer.java
@@ -3,6 +3,8 @@ package com.chengshare.usercenter.mq;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.ErrorMessage;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,10 +18,17 @@ public class TestStreamConsumer {
     @StreamListener(Sink.INPUT)
     public void receive(String messageBody) {
         log.info("收到的消息={}", messageBody);
+        throw new IllegalArgumentException("参数异常");
     }
 
     @StreamListener(MySink.MY_INPUT)
     public void receivemy(String messageBody) {
         log.info("收到的消息2my={}", messageBody);
+    }
+
+    @StreamListener("errorChannel")
+    public void error(Message<?> message) {
+        ErrorMessage errorMessage = (ErrorMessage) message;
+        log.warn("Handling ERROR: {}" ,errorMessage);
     }
 }
