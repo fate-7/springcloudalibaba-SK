e5780dc3e08f7490e388c31d5d51d49e0ceb2354
fate-7 <2651074692@qq.com>
2020-04-16 12:18:32 +0800

    SpringCloud Gateway的filter的执行顺序

--- gateway/src/main/java/com/chengshare/gateway/PreLogGatewayFilterFactory.java
+++ gateway/src/main/java/com/chengshare/gateway/PreLogGatewayFilterFactory.java
@@ -2,6 +2,7 @@ package com.chengshare.gateway;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
@@ -24,6 +25,10 @@ import org.springframework.web.server.ServerWebExchange;
  *
  * 注意点：名字一定是GatewayFilterFactory结尾
  * 配置写前缀： - PreLog=a, b a是name， b是value
+ *
+ * 过滤器执行顺序：order越小越先执行
+ * 局部过滤器从第一个配置order从1开始递增
+ * 局部过滤器自定义order -new OrderedGatewayFilter
  * @Author fate7
  * @Date 2020/4/16 11:37 上午
  **/
@@ -32,11 +37,12 @@ import org.springframework.web.server.ServerWebExchange;
 public class PreLogGatewayFilterFactory extends AbstractNameValueGatewayFilterFactory {
     @Override
     public GatewayFilter apply(NameValueConfig config) {
-        return ((exchange, chain) -> {
+        GatewayFilter filter = ((exchange, chain) -> {
             log.info("请求到达={}:{}", config.getName(), config.getValue());
             ServerHttpRequest modifiedRequest = exchange.getRequest().mutate().build();
             ServerWebExchange modifitedExchange = exchange.mutate().request(modifiedRequest).build();
             return chain.filter(modifitedExchange);
         });
+        return new OrderedGatewayFilter(filter, 10000);
     }
 }
